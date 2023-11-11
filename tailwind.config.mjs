5db4583ceb87b8dc9256ec336ee24c63e6feeb4f
Karsten Pedersen <karstenfp.all@gmail.com>
2023-11-11 19:18:43 +0100

    Add css file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      link: {
+        DEFAULT: "var(--link)",
+        visited: "var(--link-visited)",
+        active: "var(--link-active)",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
